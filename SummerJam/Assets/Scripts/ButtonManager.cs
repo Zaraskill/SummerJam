968b4a56beb0c367c8a0bc1e1d4bca23a3aeebd0
CorentinLelong <corentin.lelong77@gmail.com>
2020-07-26 12:00:48 +0200

    Change ui for display map

---
+++ SummerJam/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    private string place;
+
+    public GameObject validation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickDestination(string hotel)
+    {
+        place = hotel;
+        validation.SetActive(true);
+    }
+
+    public void OnClickValidate(bool choice)
+    {
+        if (choice)
+        {
+            if (place == GameManager.instance.goodHotelName)
+            {
+                GameManager.instance.ResultChoice(true);
+            }
+            else
+            {
+                GameManager.instance.ResultChoice(false);
+            }
+        }
+        else
+        {
+            validation.SetActive(false);
+        }
+        
+    }
+}
